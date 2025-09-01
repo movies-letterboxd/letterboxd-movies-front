602ff0bcb6466afb08604545dc94f050b60fe8b3
enzocazenave <enzocazenavedev@hotmail.com>
2025-09-01 14:01:32 -0300

    Create principal routes of app

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,17 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import MoviesPage from './pages/MoviesPage.tsx'
+import MoviePage from './pages/MoviePage.tsx'
+import NewMoviePage from './pages/NewMoviePage.tsx'
+import { Navigate } from 'react-router'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/movies" element={<MoviesPage />} />
+      <Route path="/movies/new" element={<NewMoviePage />} />
+      <Route path="/movies/:id" element={<MoviePage />} />
+      <Route path="/*" element={<Navigate to="/movies" replace />} />
+    </Routes>
+  </BrowserRouter>
+)
---
+++ src/pages/NewMoviePage.tsx
@@ -0,0 +1,5 @@
+export default function NewMoviePage() {
+  return (
+    <>NewMoviePage</>
+  )
+}
