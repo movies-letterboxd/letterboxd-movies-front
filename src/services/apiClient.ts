21eb735121c088e45febdda64ba93ed03ef222f8
Manuel Pereiro <manpereiro@uade.edu.ar>
2025-09-13 18:30:52 -0300

    Change API base URL to use HTTPS

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  baseURL: 'http://18.222.250.152:8080/api',
+  baseURL: 'https://18.222.250.152:8080/api',
   headers:{
       "Content-Type": "application/json", 
   },
