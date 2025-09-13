132fb68c892c1a68f2913e7ba293133bae7f6c52
Manuel Pereiro <manpereiro@uade.edu.ar>
2025-09-13 16:55:00 -0300

    Change API base URL to production server

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://18.222.250.152/api',
   headers:{
       "Content-Type": "application/json", 
   },
