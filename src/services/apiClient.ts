0e7ff28bf6b3f57fa62940a28a8fd9f24ea80aa9
Manuel Pereiro <manpereiro@uade.edu.ar>
2025-09-13 17:53:21 -0300

    Change API base URL to use port 8080

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  baseURL: 'http://18.222.250.152/api',
+  baseURL: 'http://18.222.250.152:8080/api',
   headers:{
       "Content-Type": "application/json", 
   },
